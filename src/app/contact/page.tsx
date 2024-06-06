1a7d2f973e4197a1be7c655254dafea7f3fbf40b
Rajshekhar Bhagat <rajbhagat27889@gmail.com>
2024-06-06 10:45:23 +0530

    making changes in page.tsx and adding the form for contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,38 +1,80 @@
-'use client'
+"use client";
 import React from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 
-function page() {
+function Page() {
+  const formSchema = z.object({
+    email: z.string().email({ message: "Invalid email address" }).nonempty({ message: "Email is required" }),
+    message: z.string().min(30, { message: "Message must be at least 30 characters long" }),
+  });
 
-    const formSchema = z.object({
-        email: z.string({
-            required_error:'Email is required'
-        }),
-        message: z.string().min(30, 'Message must me 30 character long'),
-    });
+  type FormData = z.infer<typeof formSchema>;
 
-    type FormData = z.infer<typeof formSchema>;
-
-    const form = useForm<FormData>({
-        resolver: zodR
-    })
-
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      message: "",
+    },
+  });
+  
+  const onSave = (data: FormData) => {
+    console.log(data);
+  };
+  
   return (
     <div className="min-h-screen w-full bg-black relative flex flex-col items-center justify-center antialiased">
+      <BackgroundBeams />
       <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-5xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400  text-center font-sans font-bold">
+        <h1 className="relative z-10 text-5xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400 text-center font-sans font-bold">
           Contact Us
         </h1>
-        <p className="text-neutral-400 mt-6 text-center">We are here to help with any question related to our courses, program, or events. We are here to assist you in you musical journey </p>
+        <p className="text-neutral-400 mt-6 text-center">
+          We are here to help with any question related to our courses, program,
+          or events. We are here to assist you in your musical journey.
+        </p>
         <div>
-
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSave)} className="space-y-5 mt-5 p-5 md:p-10">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder="Email" className="bg-gray-950 text-neutral-300" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder="Your message" className="bg-gray-950 text-neutral-300" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="bg-gray-900 cursor-pointer text-neutral-300 hover:bg-gray-800" type="submit">
+                Send Message
+              </Button>
+            </form>
+          </Form>
         </div>
       </div>
-      <BackgroundBeams />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,63 +1,65 @@
-'use client'
-import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card'
-import { Button } from '@/components/ui/button'
-import courseData from '@/data/music_courses.json'
-import Link from 'next/link'
+"use client";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import { Button } from "@/components/ui/button";
+import courseData from "@/data/music_courses.json";
+import Link from "next/link";
 
 function page() {
   return (
-    <div className='min-h-screen bg-black py-12 pt-36 w-full'>
-        <h1 className='text-2xl md:text-4xl text-center mb-5 font-semibold text-neutral-300'>All Courses "{courseData.courses.length}"</h1>
-        <div className='grid lg:grid-cols-2 max-w-5xl mx-auto gap-2'>
-            {courseData.courses.map((course) => (
-                <CardContainer className="inter-var">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
+    <div className="min-h-screen bg-black py-12 pt-36 w-full">
+      <h1 className="text-2xl md:text-4xl text-center mb-5 font-semibold text-neutral-300">
+        All Courses "{courseData.courses.length}"
+      </h1>
+      <div className="grid lg:grid-cols-2 max-w-5xl mx-auto gap-2">
+        {courseData.courses.map((course) => (
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                {course.title}
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                {course.description}
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <img
+                  src={course.image}
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt="thumbnail"
+                  />
+              </CardItem>
+              <div className="flex justify-between items-center mt-20">
+                <CardItem
+                  translateZ={20}
+                  as={Link}
+                  href="https://twitter.com/mannupaaji"
+                  target="__blank"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                   >
-                    {course.title}
-                  </CardItem>
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  Register now →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                   >
-                    {course.description}
-                  </CardItem>
-                  <CardItem translateZ="100" className="w-full mt-4">
-                    <img
-                      src={course.image}
-                      height="1000"
-                      width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      alt="thumbnail"
-                    />
-                  </CardItem>
-                  <div className="flex justify-between items-center mt-20">
-                    <CardItem
-                      translateZ={20}
-                      as={Link}
-                      href="https://twitter.com/mannupaaji"
-                      target="__blank"
-                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                      Register now →
-                    </CardItem>
-                    <CardItem
-                      translateZ={20}
-                      as="button"
-                      className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                    >
-                      Sign up
-                    </CardItem>
-                  </div>
-                </CardBody>
-              </CardContainer>
-            ))}
-        </div>
+                  Sign up
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
